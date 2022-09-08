bae5e6236c80aeadea731c1eeb0bf9b122c86abb
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2022-09-09 04:10:17 +1000

    Done

--- hsi_machine.py
+++ hsi_machine.py
@@ -2,23 +2,37 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 import torch
+from torchvision.models import resnet50, ResNet50_Weights
+
 
 class HsiMachine(nn.Module):
     def __init__(self):
         super().__init__()
         self.N_BANDS = 242
-        self.machine = nn.ModuleList([nn.Sequential(
-            nn.Conv2d(1, 8, (4, 4)),
-            nn.ReLU(),
-            nn.MaxPool2d((2, 2)),
-            nn.Conv2d(8, 16, (4, 4)),
-            nn.ReLU(),
-            nn.MaxPool2d((2, 2)),
-            nn.Flatten(),
-            nn.Linear(400, 16),
-            nn.ReLU(),
-            nn.Linear(16, 1)
-        ) for i in range(self.N_BANDS)])
+        self.resnets = nn.ModuleList([resnet50() for i in range(self.N_BANDS)])
+
+        for a_resnet in self.resnets:
+            number_input = a_resnet.fc.in_features
+            a_resnet.fc = nn.Sequential(
+                nn.Linear(number_input, 256),
+                nn.BatchNorm1d(256),
+                nn.ReLU(),
+                nn.Linear(256, 1),
+            )
+            weights_data = a_resnet.conv1.weight.data[:, 0]
+            weights_data = weights_data.reshape(weights_data.shape[0], 1, weights_data.shape[1], weights_data.shape[2])
+            self.resnet.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+            a_resnet.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+            a_resnet.conv1.weight.data = weights_data
+
+            for param in a_resnet.layer1.parameters():
+                param.requires_grad = False
+
+            for param in a_resnet.layer2.parameters():
+                param.requires_grad = False
+
+            for param in a_resnet.layer3.parameters():
+                param.requires_grad = False
 
         self.fc = nn.Sequential(
             nn.Linear(self.N_BANDS, 12),
